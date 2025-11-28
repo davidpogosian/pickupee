b9a61c89cb50dfcdee0e0a290509f96130f260ed
David Pogosian <davidpogosian@Davids-MacBook-Air.local>
2025-11-28 18:25:56 -0500

    Impl OrderRepository.Insert

--- platform/repository/order_repo.go
+++ platform/repository/order_repo.go
@@ -1,1 +1,34 @@
 package repository
+
+import (
+	"database/sql"
+
+	"github.com/davidpogosian/pickupee/platform/models"
+)
+
+type OrderRepository struct {
+	db sql.DB
+}
+
+// 【 Create 】
+// POST request
+func (r *OrderRepository) Insert(order *models.Order) (int, error) {
+	result, err := r.db.Exec(
+		"INSERT INTO orders (user_id) VALUES (?)",
+		order.UserID,
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+// 【 Read 】
+// 【 Update 】
+// 【 Delete 】
