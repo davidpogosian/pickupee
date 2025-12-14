8a134b0fa74fb96bcfbd4974df3e82abcd9ec058
Nightrider <david_pogosian@outlook.com>
2025-12-14 16:08:24 -0500

    Wired toggleLocker handler to the router

--- platform/router/router.go
+++ platform/router/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/davidpogosian/pickupee/service"
 	"github.com/davidpogosian/pickupee/web/api/listOrdersForUser"
 	"github.com/davidpogosian/pickupee/web/api/placeOrder"
+	"github.com/davidpogosian/pickupee/web/socket"
 )
 
 func Create(orderService *service.OrderService) *http.ServeMux {
@@ -14,6 +15,7 @@ func Create(orderService *service.OrderService) *http.ServeMux {
 	// TODO: Make a separate /orders mux
 	mux.HandleFunc("/placeOrder", placeOrder.Handler(orderService))
 	mux.HandleFunc("/orders", listOrdersForUser.Handler(orderService)) // /orders?user_id=42
+	mux.HandleFunc("/socket", socket.Handler())
 
 	return mux
 }
